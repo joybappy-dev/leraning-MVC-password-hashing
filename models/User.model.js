7153478723fa0a957b99ed7ca1a7e3aa9ad8b716
Joy Bappy <joybappy2025@gmail.com>
2026-05-27 09:18:42 +0600

    add role in user model

--- models/User.model.js
+++ models/User.model.js
@@ -14,6 +14,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   {
     timestamps: true,
